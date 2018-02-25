d4304b892208c39c86fa6e8a8e4c448e8eb7dae7
vovan2018 <vovan_vt@mail.ru>
2018-02-25 12:20:11 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Arrays;
+
+/**
+ * Created by UITS-Admin on 24.02.2018.
+ */
+public class Main {
+    public static void main(String[] args) {
+        int[] temp = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+
+        System.out.println(Arrays.toString(temp));
+    }
+}
