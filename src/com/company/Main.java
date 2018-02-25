63827946a2354a2df2813d818717e3edd900f8b2
vovan2018 <vovan_vt@mail.ru>
2018-02-25 13:22:06 +0200

    New version for asd
    (cherry picked from commit a7d06f5)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         int[] temp = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
 
-        System.out.println("New verfor asd");
+        System.out.println("New version for master");
         System.out.println(Arrays.toString(temp));
 
     }
