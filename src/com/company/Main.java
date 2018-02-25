a7d06f5d77873635c6cdcf1d32917cf15651062f
vovan2018 <vovan_vt@mail.ru>
2018-02-25 13:00:11 +0200

    New version for asd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         int[] temp = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
 
-        System.out.println("New version for asd1");
+        System.out.println("New version for asd");
         System.out.println(Arrays.toString(temp));
 
     }
